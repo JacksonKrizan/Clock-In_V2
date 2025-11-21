93e04b486b8fa54fd9262e70029967c68db05a4e
JacksonKrizan <72949242+JacksonKrizan@users.noreply.github.com>
2025-11-21 00:51:40 -0500

    test

--- Assets/Scripts/NetworkMapCode/Clickable.cs
+++ Assets/Scripts/NetworkMapCode/Clickable.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Clickable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] List<GameObject> objectsToToggle;
+    [SerializeField] List<string> tagsToToggle = new List<string>();
+
+
+    void Update()
+    { Debug.Log("1 Clickable");
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {Debug.Log("2 Clickable");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {Debug.Log("3 Clickable");
+                //Transform hitT = hit.collider.transform;
+                for (int i = 0; i < tagsToToggle.Count; i++)
+                {Debug.Log("4 Clickable");
+                    Debug.Log(hit.collider.tag);
+                    //var target = mapObjects[i];
+                    
+                    //if (hitT == target.transform || hitT.IsChildOf(target.transform))
+                    if (hit.collider.CompareTag(tagsToToggle[i]))
+                    {
+                        Debug.Log("5 Clickable");
+                        //SelectMap(i);
+                        Debug.Log("Clicked on " + tagsToToggle[i]);
+                        objectsToToggle[i].SetActive(!objectsToToggle[i].activeSelf);
+                        return;
+                    }
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
