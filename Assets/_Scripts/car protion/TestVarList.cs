f467d0ae6d9aa5e4da8b063f170c86387f0316f2
JacksonKrizan <72949242+JacksonKrizan@users.noreply.github.com>
2026-01-27 14:45:01 -0500

    added some code

--- Assets/_Scripts/car protion/TestVarList.cs
+++ Assets/_Scripts/car protion/TestVarList.cs
@@ -4,17 +4,5 @@ using UnityEngine;
 
 public class TestVarList : MonoBehaviour
 {
-    public List<KeyValuePair<string, string>> carParts = new List<KeyValuePair<string, string>>();
-    List<KeyValuePair<string, int>> playerScores = new List<KeyValuePair<string, int>>();
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public List<GameObject> partList = new List<GameObject>();
 }
