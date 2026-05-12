68c155b6ec60d52ce47d8c140351ee9c6c59b6e1
JacksonKrizan <72949242+JacksonKrizan@users.noreply.github.com>
2026-05-11 21:58:07 -0400

    broken prefab etc

--- Assets/_Scripts/_GameImportant/PlayerImportantScripts/PlayerManager.cs
+++ Assets/_Scripts/_GameImportant/PlayerImportantScripts/PlayerManager.cs
@@ -5,25 +5,35 @@ using Photon.Pun;
 using System.IO;
 using System.Linq;
 using Photon.Realtime;
-using Hashtable = ExitGames.Client.Photon.Hashtable;
-using System.Numerics;
-
-
+using UnityEngine.SceneManagement;
 
 public class PlayerManager : MonoBehaviourPunCallbacks
 {
     PhotonView PV;
     GameObject controller;
 
-    Launcher launcher;
-    string prefabPath = "PhotonPrefabs/PlayerController"; 
     private void Awake()
     {
         PV = GetComponent<PhotonView>();
     }
 
-    void Start()
+    public override void OnEnable()
     {
+        base.OnEnable();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Don't spawn if we are in the Menu
+        if (scene.name == "_Menu" || scene.name == "Menu") return;
+
         if (PV.IsMine)
         {
             CreateController();
@@ -32,118 +42,33 @@ public class PlayerManager : MonoBehaviourPunCallbacks
 
     void CreateController()
     {
-        GetPlayerController();
-        /*if (launcher.mapNumber == 0)
+        string sceneName = SceneManager.GetActiveScene().name;
+        string prefabName = "PlayerController"; // Default fallback
+
+        // Select the specific prefab for this map based on actual file names
+        if (sceneName == "AutoShop")
         {
-            //Debug.Log("Map number is 0, using GameDev prefab");
-            prefabPath = "PhotonPrefabs/GamingPlayerController";
+            prefabName = "AutoPlayerController";
         }
-        else
+        else if (sceneName == "Chef")
         {
-            prefabPath = "PhotonPrefabs/PlayerController";
-        }*/
-        Debug.Log("Creating player controller for " + PV.Owner.NickName);
+            prefabName = "FirePlayerController";
+        }
+        else if (sceneName == "GameDev")
+        {
+            prefabName = "GamingPlayerController";
+        }
+
+        //Debug.Log($"[PlayerManager] Map detected: {sceneName}. Choosing prefab: {prefabName}");
+
+        Vector3 spawnPos = Vector3.zero;
+        Quaternion spawnRot = Quaternion.identity;
         Transform spawnpoint = SpawnManager.Instance.GetSpawnpoint();
-        controller = PhotonNetwork.Instantiate(prefabPath, spawnpoint.position, spawnpoint.rotation, 0, new object[] { PV.ViewID });
+        controller = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs/", prefabName), spawnPos, spawnRot, 0, new object[] { PV.ViewID });
     }
-    void GetPlayerController()
-    {
-        if (launcher.mapNumber == 0)
-        {
-            //Debug.Log("Map number is 0, using GameDev prefab");
-            prefabPath = "PhotonPrefabs/GamingPlayerController";
-        }
-        else if (launcher.mapNumber == 1)
-        {
-            prefabPath = "PhotonPrefabs/AutoPlayerController";
-        }
-        else if (launcher.mapNumber == 2)
-        {
-            prefabPath = "PhotonPrefabs/FirePlayerController";
-        }
-        else
-        {
-            prefabPath = "PhotonPrefabs/PlayerController";
-        }
-        
-    }
-    /*void CreateController()
-    {
-        //string prefabPath = "PhotonPrefabs/GamingPlayerController"; // Default path
-        string prefabPath = "PhotonPrefabs/PlayerController"; // Default path
-        
-        Debug.Log("Creating player controller for " + PV.Owner.NickName);
-        Transform spawnpoint = SpawnManager.Instance.GetSpawnpoint();
-        if (launcher.mapNumber == 0)
-        {
-            //Debug.Log("Map number is 0, using GameDev prefab");
-            prefabPath = "PhotonPrefabs/GamingPlayerController";
-        }
-        else if (launcher.mapNumber == 1)
-        {
-            prefabPath = "PhotonPrefabs/AutoPlayerController";
-        }
-        else if (launcher.mapNumber == 2)
-        {
-            prefabPath = "PhotonPrefabs/FirePlayerController";
-        }
 
-        controller = PhotonNetwork.Instantiate(prefabPath, spawnpoint.position, spawnpoint.rotation, 0, new object[] { PV.ViewID });
-    }*/
-
-    /*void CreateController()
-    {
-        string sceneName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
-        string prefabPath = "PhotonPrefabs/PlayerController"; // Default path
-
-        //if (sceneName == "GameDev")
-        if (launcher.mapNumber == 0)
-        {
-            Debug.Log("Map number is 0, using GameDev prefab");
-            prefabPath = "PhotonPrefabs/GamingPlayerController";
-        }
-        else if (sceneName == "WarehouseScene")
-        {
-            prefabPath = "PhotonPrefabs/WarehousePlayerController";
-        }
-        else if (sceneName == "FactoryScene")
-        {
-            prefabPath = "PhotonPrefabs/FactoryPlayerController";
-        }
-        Debug.Log("Creating player controller for " + PV.Owner.NickName + " using prefab: " + prefabPath);
-
-
-
-        if (SpawnManager.Instance != null)
-        {
-            Transform spawnpoint = SpawnManager.Instance.GetSpawnpoint();
-            controller = PhotonNetwork.Instantiate(prefabPath, spawnpoint.position, spawnpoint.rotation, 0, new object[] { PV.ViewID });
-        }
-        else
-        {
-            Debug.LogWarning("SpawnManager instance not found! Spawning at default position.");
-        }
-        
-    }*/
-    
-    public override void OnPlayerEnteredRoom(Player newPlayer)
-    {
-        //Debug.Log("New Player Joined: " + newPlayer.NickName);
-
-        
-        if (PhotonNetwork.IsMasterClient)
-        {
-            foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player"))
-            {
-                PhotonView pv = player.GetComponent<PhotonView>();
-                pv.RPC("SyncExistingPlayer", newPlayer);
-                Debug.Log("Syncing player for new player");
-            }
-        }
-    }
     public static PlayerManager Find(Player player)
     {
         return FindObjectsOfType<PlayerManager>().SingleOrDefault(x => x.PV.Owner == player);
     }
-
 }
