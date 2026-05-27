3e7663363378bb404112f4cc32c85c5a535fdf8c
JacksonKrizan <72949242+JacksonKrizan@users.noreply.github.com>
2026-05-27 14:03:04 -0400

    make button work and placement on packets

--- Assets/_Scripts/GamingMap/PacketGoal.cs
+++ Assets/_Scripts/GamingMap/PacketGoal.cs
@@ -5,18 +5,13 @@ public class PacketGoal : MonoBehaviour
 {
     public string packetTag = "Packages";
 
-    void OnTriggerEnter(Collider other)
+    void OnCollisionEnter(Collision collision)
     {
         // Check if the object entering the trigger is a Packet
-        if (other.CompareTag(packetTag))
+        if (collision.gameObject.CompareTag(packetTag))
         {
-            Packet packet = other.GetComponent<Packet>();
-            if (packet != null)
-            {
-                // Trigger the delivery across the network
-                packet.photonView.RPC("Deliver", RpcTarget.AllBuffered);
-                Debug.Log("Packet reached the delivery goal!");
-            }
+            //Packet packet = other.GetComponent<Packet>();
+            Debug.Log("Goal");
         }
     }
 }
