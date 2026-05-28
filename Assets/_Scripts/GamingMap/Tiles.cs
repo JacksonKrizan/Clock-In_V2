ca43736c5d6287b66e0c4a5b0a58d9c9790e6206
JacksonKrizan <72949242+JacksonKrizan@users.noreply.github.com>
2026-05-28 14:46:45 -0400

    timer

--- Assets/_Scripts/GamingMap/Tiles.cs
+++ Assets/_Scripts/GamingMap/Tiles.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Tiles : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public List<GameObject> targets = new List<GameObject>();
+    public Color glowColor = Color.cyan;
 
-    // Update is called once per frame
+    public float speed = UnityEngine.Random.Range(1.5f, 5.0f);
+
+
+
+        void Start() 
+        {
+            
+            InvokeRepeating("TilesFaded", speed);
+        }
+
+        void TilesFaded() 
+        {
+            speed = UnityEngine.Random.Range(1.5f, 5.0f);
+            Debug.Log("Executed every 5 seconds!");
+        }
+
+
     void Update()
     {
-        
+
+
+
+
+
+
+        float intensity = Mathf.PingPong(Time.time * speed, 1f);
+        foreach (var go in targets)
+        {
+            if (go == null) continue;
+            foreach (var r in go.GetComponentsInChildren<Renderer>())
+            {
+                var mat = r.material;
+                if (mat && mat.HasProperty("_EmissionColor"))
+                    mat.SetColor("_EmissionColor", glowColor * intensity);
+            }
+        }
     }
 }
